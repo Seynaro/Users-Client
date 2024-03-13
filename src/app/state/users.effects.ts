d0930ce1e9d6352645a4942bcc69e596dc171085
Khamzat <xamzatseynaro@gmail.com>
2024-03-13 13:46:45 +0300

    step 2 - add catchError in effects

--- src/app/state/users.effects.ts
+++ src/app/state/users.effects.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Actions, createEffect, ofType} from '@ngrx/effects';
 import * as UserActions from "./users.actions";
-import {map, of, switchMap} from "rxjs";
+import {map, of, switchMap, catchError} from "rxjs";
 import {UserApiService} from "../services/user-api.service";
 import {editUser} from "./users.actions";
 
@@ -17,30 +17,33 @@ export class UserEffects {
       ofType(UserActions.loadUsers),
       switchMap(() =>
         this.userApi.getUsers().pipe(
-          map(userList => UserActions.loadUsersSuccess({users: userList})
-          )))))
+          map(userList => UserActions.loadUsersSuccess({users: userList})),
+          catchError(error => of(UserActions.loadUsersFail({error})))
+        ))))
 
   delete$ = createEffect(() =>
     this.actions$.pipe(
       ofType(UserActions.deleteUser),
       switchMap(({deletedUser}) =>
         of(this.userApi.deleteUser(deletedUser)).pipe(
-          map(() => UserActions.deleteUserSuccess({deletedUser})
-          )))))
+          map(() => UserActions.deleteUserSuccess({deletedUser})),
+          catchError(error => of(UserActions.loadUsersFail({error})))))))
 
   addUser$ = createEffect(() =>
     this.actions$.pipe(
       ofType(UserActions.addUser),
       switchMap(({newUser}) =>
         of(this.userApi.addUser(newUser)).pipe(
-          map(() => UserActions.addUserSuccess({newUser})
-          )))))
+          map(() => UserActions.addUserSuccess({newUser})),
+          catchError(error => of(UserActions.loadUsersFail({error})))
+        ))))
 
   editUser$ = createEffect(() =>
     this.actions$.pipe(
       ofType(editUser),
       switchMap(({editedUser}) =>
         of(this.userApi.editUser(editedUser)).pipe(
-          map(() => UserActions.editUserSuccess({editedUser})
-          )))))
+          map(() => UserActions.editUserSuccess({editedUser})),
+          catchError(error => of(UserActions.loadUsersFail({error})))
+        ))))
 }
